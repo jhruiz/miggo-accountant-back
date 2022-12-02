def4f6c4beb3c903c2388ed52b5d154642553da2
jhruiz <jaiber.ruiz@hotmail.com>
2022-12-02 10:54:00 -0500

    se ajusta la respuesta del index de menu para retornar un json ordenado entre padres e hijos

--- app/Http/Controllers/Cloudmenu/CloudmenuController.php
+++ app/Http/Controllers/Cloudmenu/CloudmenuController.php
@@ -13,7 +13,24 @@ class CloudmenuController extends ApiController
     public function index()
     {
         $cloudmenus = Cloudmenu::all();
-        return $this->showAll($cloudmenus);
+        
+        $arrClMenu = [];
+        foreach( $cloudmenus as $val ) {
+            if( !empty($val['cloudmenu_id']) ){
+
+                $arrClMenu[$val['cloudmenu_id']]['hijos'][$val['id']] = $val['descripcion'];
+                $arrClMenu[$val['cloudmenu_id']]['hijos'][$val['id']] = $val['url'];
+                $arrClMenu[$val['cloudmenu_id']]['hijos'][$val['id']] = $val['imagen'];
+                $arrClMenu[$val['cloudmenu_id']]['hijos'][$val['id']] = $val['ayuda'];
+
+            } else {
+
+                $arrClMenu[$val['id']]['descripcion'] = $val['descripcion']; 
+
+            }
+        }
+
+        return response()->json($arrClMenu);
     }
 
 }
